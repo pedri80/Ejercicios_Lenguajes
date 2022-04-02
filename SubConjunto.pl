2492ff19b6c39fef1609dcdbbf539762ba6dd4d9
pedri80 <49495965+pedri80@users.noreply.github.com>
2022-04-01 20:42:35 -0600

    Punto 3

--- SubConjunto.pl
+++ SubConjunto.pl
@@ -1,18 +1,25 @@
-%3)Defina la relación subconj(S, S1), donde S y S1 son listas
+%3)Defina la relaciÃ³n subconj(S, S1), donde S y S1 son listas
 % representando conjuntos, que es verdadera si S1 es subconjunto de S.
 subconj([S|Tail],[S1|Cola]):-
     %Compara elementos de la cabeza
-    S is S1,
-    %Corta los elementos y sigue adelante
-    subconj(Tail,Cola);
-    %Se corta el patron sublista
-    subconj(Tail, [S1|Cola]).
-
-subconj([],[]).
+    comprueba([S|Tail], [S1|Cola], [S1|Cola]).
 
 %Se encuentra la sublista
-subconj([_|Tail],[]):-
-    subconj(Tail,[]).
+comprueba([_|Tail],[], []):-
+    comprueba(Tail,[], []).
 
+comprueba([],[], [_|Aux]):-
+    comprueba([],[], Aux).
 
+comprueba([],[], []).
 
+comprueba([],[_|Tail], [_|Aux]):-
+    comprueba([], Tail, Aux),
+    false.
+
+comprueba([S|Tail], [S1|Cola], [Aux|Listado]):-
+    S is S1,
+    %Corta los elementos y sigue adelante
+    comprueba(Tail, Cola, [Aux|Listado]);
+    %Se corta el patron sublista
+    comprueba(Tail, [Aux|Listado], [Aux|Listado]).
